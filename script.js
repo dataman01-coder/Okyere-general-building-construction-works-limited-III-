380c9f1c88ae291deee5f4f2ad361b9e324b75c7
dataman_01-coder <dataman@gmail.com>
2025-04-27 20:36:17 -0700

    All files added to the local repo and am about to fire them to the remote repo..se you there

---
+++ script.js
@@ -0,0 +1,9 @@
+var navlinks = document.getElementById("nav-links");
+
+function closeMenu(){
+    navlinks.style.display = "none";
+}
+
+function hideMenu(){
+    navlinks.style.display = "block";
+}
